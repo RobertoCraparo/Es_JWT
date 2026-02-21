f35dc8d931a52bfdefaa602d3cde395b6135bcbe
AkiraKage <alok95ita@gmail.com>
2026-02-21 10:56:51 +0100

    completati form per invio e ricezione in app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const API_BASE = 'http://localhost/api-jwt'; // es: http://localhost/api
 
 const form = document.getElementById('loginForm');
+const regForm = document.getElementById('regForm');
 const output = document.getElementById('output');
 
 form.addEventListener('submit', async (e) => {
@@ -32,6 +33,36 @@ form.addEventListener('submit', async (e) => {
         output.textContent = err.message;
     }
 });
+
+regForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch(`${API_BASE}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.error || 'Errore di registrazione');
+        }
+
+        // salva JWT
+        localStorage.setItem('token', data.token);
+
+        output.textContent = 'Registrazione effettuata con successo';
+    } catch (err) {
+        output.textContent = err.message;
+    }
+})
 
 async function getUsers() {
     const token = localStorage.getItem('token');
